9cd7a4ec52c4e5ef5cc1372aee6b0a9dae49e02b
manu760app <manu_levelz1@yahoo.com.ar>
2023-03-01 09:26:31 -0300

    1 commit

---
+++ JSPractice.js
@@ -0,0 +1,14 @@
+//https://www.boredapi.com/api/
+
+let movies = {list: ['Antman', 'Superman', 'Batman']};
+
+function getMovies(movies){
+  setTimeout(() => {
+    console.log(movies.list);
+  },3000);
+}
+function f(){
+  console.log('Waiting...');
+}
+getMovies(movies);
+f();
